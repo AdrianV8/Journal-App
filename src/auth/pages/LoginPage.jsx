852a821839d0b09aa764b1da6b5e3bae788628fe
Adrian <alumnoadrian9@gmail.com>
2023-01-11 09:53:45 +0100

    AuthLayout hecho, creada página de registro de usuario, pequeños cambios en la página de registro

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,28 +2,13 @@ import { Link as RouterLink } from 'react-router-dom'
 import { Google } from '@mui/icons-material'
 import { Button, Grid, TextField, Typography, Link } from '@mui/material'
 import React from 'react'
+import { AuthLayout } from '../layout/AuthLayout'
 
 export const LoginPage = () => {
   return (
     
-    <Grid 
-      container 
-      spacing={ 0 } 
-      direction="column" 
-      alignItems="center" 
-      justifyContent="center"
-      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
-    >
-      
-      <Grid 
-        item
-        className='box-shadow'
-        xs={3}
-        sx={{ backgroundColor:'white', padding: 3, borderRadius: 2 }}
-      >
-        <Typography variant='h5' sx={{ mb: 1 }}>
-          Login
-        </Typography>
+      <AuthLayout title='Login'>
+        
         <form>
 
           <Grid container>
@@ -31,7 +16,7 @@ export const LoginPage = () => {
               <Grid item xs={12} sx={{ mt: 2 }}>
                 <TextField label="Correo" type={'email'} placeholder="Correo@google.com" fullWidth/>
               </Grid>
-        
+
               <Grid item xs={12} sx={{ mt: 2 }}>
                 <TextField label="Contraseña" type={'password'} placeholder="Contraseña" fullWidth/>
               </Grid>
@@ -61,9 +46,7 @@ export const LoginPage = () => {
 
         </form>
 
-      </Grid>
-
-    </Grid>
+      </AuthLayout>
     
   )
 }
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,50 @@
+import { Link as RouterLink } from 'react-router-dom'
+import { Google } from '@mui/icons-material'
+import { Button, Grid, TextField, Typography, Link } from '@mui/material'
 import React from 'react'
+import { AuthLayout } from '../layout/AuthLayout'
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    
+      <AuthLayout title='Crea una cuenta'>
+        
+        <form>
+
+          <Grid container>
+
+              <Grid item xs={12} sx={{ mt: 2 }}>
+                <TextField label="Nombre completo" type={'text'} placeholder="Nombre completo" fullWidth/>
+              </Grid>
+              
+              <Grid item xs={12} sx={{ mt: 2 }}>
+                <TextField label="Correo" type={'email'} placeholder="Correo@google.com" fullWidth/>
+              </Grid>
+
+              <Grid item xs={12} sx={{ mt: 2 }}>
+                <TextField label="Contraseña" type={'password'} placeholder="Contraseña" fullWidth/>
+              </Grid>
+              
+              <Grid container spacing={2} sx={{ mt: 1 }}>
+                <Grid item xs={12} >
+                  <Button variant='contained' fullWidth>
+                    Crear cuenta
+                  </Button>
+                </Grid>
+                
+              </Grid>
+
+          </Grid>
+
+          <Grid container direction={'row'} justifyContent={'end'} sx={{ mt: 1 }}>
+            
+            <Link component={RouterLink} color='inherit' to="/auth/login">¿Ya tienes un usuario?</Link>
+            
+          </Grid>
+
+        </form>
+
+      </AuthLayout>
+    
   )
 }
