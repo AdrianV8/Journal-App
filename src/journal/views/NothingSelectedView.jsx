c7f7a379f339a943876c4b44cfd2c0b69867b8d2
Adrian <alumnoadrian9@gmail.com>
2023-01-17 13:34:54 +0100

    Vista 'NothingSelectedView' creada

--- src/journal/views/NothingSelectedView.jsx
+++ src/journal/views/NothingSelectedView.jsx
@@ -1,4 +1,4 @@
-import { StarOutline } from "@mui/icons-material"
+import { Assignment, StarOutline } from "@mui/icons-material"
 import { Grid, Typography } from "@mui/material"
 
 export const NothingSelectedView = () => {
@@ -12,7 +12,7 @@ export const NothingSelectedView = () => {
       sx={{ minHeight: 'calc(100vh - 110px)', backgroundColor: 'primary.main', borderRadius: '10px'}}
     >
       <Grid item xs={12}>
-          <StarOutline sx={{fontSize: 100, color: 'white'}}/>
+          <Assignment sx={{fontSize: 100, color: 'white'}}/>
       </Grid>
       <Grid item xs={12}>
           <Typography color='white' variant="h5" >Selecciona o crea una entrada</Typography>
