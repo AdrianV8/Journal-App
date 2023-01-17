207d1e5dec0ade8d686c226fddd9cff89ba9b69f
Adrian <alumnoadrian9@gmail.com>
2023-01-17 14:16:55 +0100

    Implementado Redux en Auth

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const authSlice = createSlice({
+    name: 'counter',
+    initialState: {
+        status: false,
+        uid: null,
+        email: null,
+        displayName: null,
+        photoURL: null,
+        errorMessage: null,
+    },
+    reducers: {
+
+        // Login usuario
+        login: (state, action) => {
+            
+        }, 
+
+        // Logout usuario
+        logout: (state, payload) => {
+            
+        },
+
+        // Verificar si el usuario está autenticado o no
+        checkingCredentials: (state) => {
+
+        }
+    }
+})
+// Action creators are generated for each case reducer function
+export const { login, logout, checkingCredentials } = authSlice.actions;
